43b06ca96b91d5811a80438c453f944b0e3eeceb
Joshua McDonald <mcdojj12@gmail.com>
2021-02-27 18:39:03 +1000

    wip

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test change 3
+          change text woooo
         </p>
         <a
           className="App-link"
