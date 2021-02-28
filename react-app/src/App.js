4bee794b7694779760e6b2d5b111c31ed336b9e2
Joshua McDonald <mcdojj12@gmail.com>
2021-02-28 14:12:07 +1000

    wip

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          change text woooo push to main, no tag push
+          change text woooo push to main, no tag push 
+          this is another change
         </p>
         <a
           className="App-link"
