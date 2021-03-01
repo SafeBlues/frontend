6b626c614a10ce43e96ce0393adedda8670359eb
Joshua McDonald <mcdojj12@gmail.com>
2021-03-01 22:00:03 +1000

    removed lines needed for github deploy

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/frontend/dist", // this causes blank page on build? was this a github pages fix? --- part of vue deploy instructions 
   transpileDependencies: ["vuetify"]
 }
